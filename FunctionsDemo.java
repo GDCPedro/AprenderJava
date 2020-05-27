d1c196d26f656fbbfa6a0b91828ece6058c72718
郭东超 <1252120304@qq.com>
2020-05-27 17:29:22 +0800

    feat(方法Function) 方法的重载

--- FunctionsDemo.java
+++ FunctionsDemo.java
@@ -1,9 +1,15 @@
 public class FunctionsDemo {
   public static void main(String[] args) {
     System.out.println(max(22, 33));
+
+    System.out.println(max(3.1431, 3.13));
   }
 
   private final static int max(int num1, int num2) {
+    return num1 >= num2 ? num1 : num2;
+  }
+
+  private final static double max(double num1, double num2) {
     return num1 >= num2 ? num1 : num2;
   }
 }
